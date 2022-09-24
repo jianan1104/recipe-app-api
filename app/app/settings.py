4803f4aa39eaa93c09861d855333fb597dd262a3
jianan1104 <linjianan1104@gmail.com>
2022-09-25 01:11:36 +0800

    build: Setup docker file

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'changeme')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
-        os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
     )
 )
 
